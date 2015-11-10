8061d16c0fb6d9a9fc570beee68b7cd8dbd8ef98
Ashley Dawson <ashley@ashleydawson.co.uk>
2015-11-10 14:46:20 +0000

    Update StorableFieldMapper.php

--- src/ORM/Mapping/StorableFieldMapper.php
+++ src/ORM/Mapping/StorableFieldMapper.php
@@ -16,43 +16,51 @@ class StorableFieldMapper implements StorableFieldMapperInterface
      */
     public function mapFields(ClassMetadataInfo $classMetadata)
     {
-        $classMetadata
-            ->mapField([
-                'fieldName' => 'fileName',
-                'columnName' => 'file_name',
-                'type' => 'string',
-                'length' => 255,
-                'nullable' => true,
-            ])
-        ;
+        if (! $classMetadata->hasField('fileName')) {
+            $classMetadata
+                ->mapField([
+                    'fieldName' => 'fileName',
+                    'columnName' => 'file_name',
+                    'type' => 'string',
+                    'length' => 255,
+                    'nullable' => true,
+                ])
+            ;
+        }
 
-        $classMetadata
-            ->mapField([
-                'fieldName' => 'fileStoragePath',
-                'columnName' => 'file_storage_path',
-                'type' => 'string',
-                'length' => 255,
-                'nullable' => true,
-            ])
-        ;
+        if (! $classMetadata->hasField('fileStoragePath')) {
+            $classMetadata
+                ->mapField([
+                    'fieldName' => 'fileStoragePath',
+                    'columnName' => 'file_storage_path',
+                    'type' => 'string',
+                    'length' => 255,
+                    'nullable' => true,
+                ])
+            ;
+        }
 
-        $classMetadata
-            ->mapField([
-                'fieldName' => 'fileSize',
-                'columnName' => 'file_size',
-                'type' => 'integer',
-                'nullable' => true,
-            ])
-        ;
+        if (! $classMetadata->hasField('fileSize')) {
+            $classMetadata
+                ->mapField([
+                    'fieldName' => 'fileSize',
+                    'columnName' => 'file_size',
+                    'type' => 'integer',
+                    'nullable' => true,
+                ])
+            ;
+        }
 
-        $classMetadata
-            ->mapField([
-                'fieldName' => 'fileMimeType',
-                'columnName' => 'file_mime_type',
-                'type' => 'string',
-                'length' => 60,
-                'nullable' => true,
-            ])
-        ;
+        if (! $classMetadata->hasField('fileMimeType')) {
+            $classMetadata
+                ->mapField([
+                    'fieldName' => 'fileMimeType',
+                    'columnName' => 'file_mime_type',
+                    'type' => 'string',
+                    'length' => 60,
+                    'nullable' => true,
+                ])
+            ;
+        }
     }
 }
